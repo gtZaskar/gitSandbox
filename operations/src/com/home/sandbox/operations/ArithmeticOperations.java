54a9aa1ad9843e11e89fd3a093532971049a73f6
gtZaskar <gtZaskarProLe@gmail.com>
2020-10-23 15:25:50 +0300

    added directory operations

---
+++ operations/src/com/home/sandbox/operations/ArithmeticOperations.java
@@ -0,0 +1,35 @@
+package com.home.sandbox.operations;
+
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+        //arithmetic operations on integer values
+        System.out.println("integer arithmetic");
+        int a = 1+1;
+        int b = a*3;
+        int c = b/4;
+        int d = c-a;
+        int e = - d;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+        System.out.println("e = " + e);
+
+        // arithmetic operations on double values
+        System.out.println();
+        System.out.println("double arithmetic");
+        double da = 1+1;
+        double db = da*3;
+        double dc = db/4;
+        double dd = dc-a;
+        double de = -dd;
+        System.out.println("da = " + da);
+        System.out.println("db = " + db);
+        System.out.println("dc = " + dc);
+        System.out.println("dd = " + dd);
+        System.out.println("de = " + de);
+
+
+
+    }
+}
---
+++ operations/src/com/home/sandbox/operations/ModulusDivision.java
@@ -0,0 +1,11 @@
+package com.home.sandbox.operations;
+
+public class ModulusDivision {
+    public static void main(String[] args) {
+        //modulus division use demonstration %
+        int x = 42;
+        double y = 42.25;
+        System.out.println("x mod 10 = " + x % 10 );
+        System.out.println("y mod 10 =" + y % 10);
+    }
+}
