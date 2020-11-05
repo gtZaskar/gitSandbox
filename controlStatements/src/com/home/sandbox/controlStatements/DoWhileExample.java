891b3c6e731da6893f3d71d1a6b54f2797ec1656
gtZaskar <gtZaskarProLe@gmail.com>
2020-11-05 17:07:50 +0200

    added some do-while classes example

---
+++ controlStatements/src/com/home/sandbox/controlStatements/DoWhileExample.java
@@ -0,0 +1,11 @@
+package com.home.sandbox.controlStatements;
+
+public class DoWhileExample { //page142
+    public static void main(String[] args) {
+        int counter = 10;
+
+        do {
+            System.out.println(counter);
+        }while (--counter>0);
+    }
+}
